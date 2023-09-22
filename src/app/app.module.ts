eb899c5bed458fc5c5c3e9f2111e4b6134d4d7cb
NeDonil <svinoukhov03@gmail.com>
2023-09-22 21:05:47 +0300

    feat(MaterialTable): material table example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { FormsModule} from '@angular/forms';
 import { TableStudentsComponent } from './components/table-students/table-students.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { DialogEditWrapperComponent } from './components/student-editor/dialog-edit-wrapper/dialog-edit-wrapper.component';
@@ -15,13 +15,17 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './service/in-memory-data.service';
 import {MatButtonModule} from '@angular/material/button';
 import { StudentEditorWrapperComponent } from './components/student-editor/student-editor-wrapper/student-editor-wrapper.component';
+import { MatTableStudentComponent } from './components/mat-table-student/mat-table-student.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableStudentsComponent,
     DialogEditWrapperComponent,
-    StudentEditorWrapperComponent
+    StudentEditorWrapperComponent,
+    MatTableStudentComponent
   ],
   imports: [
     FormsModule,
@@ -33,7 +37,8 @@ import { StudentEditorWrapperComponent } from './components/student-editor/stude
     MatFormFieldModule,
     MatInputModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false})
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false}),
+    MatTableModule, MatPaginatorModule
   ],
   providers: [
     { provide: MAT_DIALOG_DATA, useValue: {} },
