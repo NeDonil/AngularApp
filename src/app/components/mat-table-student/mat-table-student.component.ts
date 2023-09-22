9997a7d168453d8f4352e723efb12813919aff30
NeDonil <svinoukhov03@gmail.com>
2023-09-22 21:51:29 +0300

    feat(MatTable): edit/delete button

--- src/app/components/mat-table-student/mat-table-student.component.ts
+++ src/app/components/mat-table-student/mat-table-student.component.ts
@@ -5,6 +5,9 @@ import { Student } from 'src/app/model/Student';
 import { BaseServiceService } from 'src/app/service/base-service.service';
 import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { DialogEditWrapperComponent } from '../student-editor/dialog-edit-wrapper/dialog-edit-wrapper.component';
+import { StudentEditorWrapperComponent } from '../student-editor/student-editor-wrapper/student-editor-wrapper.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-mat-table-student',
@@ -13,11 +16,12 @@ import { LiveAnnouncer } from '@angular/cdk/a11y';
 })
 
 export class MatTableStudentComponent implements AfterViewInit {
-    displayedColumns: string[] = ['id', 'name', 'surname'];
+    displayedColumns: string[] = ['actions', 'id', 'name', 'surname'];
     dataSource = new MatTableDataSource<Student>([]);
   
     constructor(private baseService: BaseServiceService,
-        private _liveAnnouncer: LiveAnnouncer){
+        private _liveAnnouncer: LiveAnnouncer,
+        public dialog: MatDialog){
     }
 
     @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -35,5 +39,40 @@ export class MatTableStudentComponent implements AfterViewInit {
         } else {
           this._liveAnnouncer.announce('Sorting cleared');
         }
-      }
+    }
+
+    addNewStudent(){
+        const dialogAddingNewStudent = this.dialog.open(DialogEditWrapperComponent, {
+            width: "400px",
+            data: null
+        });
+
+        dialogAddingNewStudent.afterClosed().subscribe((result: Student) => {
+            if(result != null){
+            this.baseService.addNewStudent(result).subscribe(k =>
+                this.baseService.getAllStudents().subscribe(data => this.dataSource.data = data));
+            }
+        });
+    }
+
+    editStudent(student: Student){
+        const studentEditingStudent = this.dialog.open(StudentEditorWrapperComponent, {
+            width: "400px",
+            data: {id: student.id, name: student.name, surname: student.surname}
+        });
+
+        studentEditingStudent.afterClosed().subscribe((student: Student) => {
+            if(student != null){
+                this.baseService.editStudent(student).subscribe(k =>
+                    this.baseService.getAllStudents().subscribe(data => this.dataSource.data = data));
+            }
+        });
+    }
+
+    deleteStudent(studentId: number){
+        if(studentId != null){
+            this.baseService.deleteStudent(studentId).subscribe(k =>
+                this.baseService.getAllStudents().subscribe(data => this.dataSource.data = data));;
+        } 
+    }
   }
